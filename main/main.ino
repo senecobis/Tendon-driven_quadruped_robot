7169539fc632e4ef54b7e4705c9979db60d9f1d1
Roberto Pellerito <rpellerito@student.ethz.ch>
2024-11-24 23:21:07 +0100

    last updates

--- main/main.ino
+++ main/main.ino
@@ -16,9 +16,36 @@ Servo right_front_upper;
 Servo right_front_lower;
 Servo right_front_feet;
 
-const int middle = 90;
-const int start = 0;
-const int mov_delay = 10;
+Servo servos[] = {
+  left_back_upper,
+  left_back_lower,
+  left_back_feet,
+  right_back_upper,
+  right_back_lower,
+  right_back_feet,
+  left_front_upper,
+  left_front_lower,
+  left_front_feet,
+  right_front_upper,
+  right_front_lower,
+  right_front_feet
+};
+
+
+const char* servos_name[] = {
+  "left_back_upper", 
+  "left_back_lower",
+  "left_back_feet",
+  "right_back_upper",
+  "right_back_lower",
+  "right_back_feet",
+  "left_front_upper",
+  "left_front_lower",
+  "left_front_feet",
+  "right_front_upper",
+  "right_front_lower",
+  "right_front_feet"
+};
 
 void setup() {
   left_back_upper.attach(12); 
@@ -38,9 +65,23 @@ void setup() {
   right_front_feet.attach(13);
 
   Serial.begin(9600);
-
   set_aestetic_pose();
-  delay(100);
+}
+
+
+void span_leg_poses(){
+  for(int pos = 0; pos <= 180; pos++){
+    left_back_upper.write(pos);
+    left_back_lower.write(pos);
+    set_constrained_feet_pos(pos, left_back_feet);
+    delay(10);
+  };
+  for(int pos = 180; pos >= 0; pos--){
+    left_back_upper.write(pos);
+    left_back_lower.write(pos);
+    set_constrained_feet_pos(pos, left_back_feet);
+    delay(10);
+  };
 }
 
 
@@ -97,16 +138,44 @@ void set_aestetic_pose(){
 
 }
 
+
 void loop() {
     //set_default_pose();
     // set_aestetic_pose();
     // delay(1000);
 
+  // if (Serial.available() > 0) {
+  //   int angle = Serial.parseInt();  // Read angle from serial input
+  //   right_front_lower.write(angle);         // Move servo to the specified angle
+  //   Serial.print("Servo moved to: ");
+  //   Serial.println(angle);
+  // }
+
   if (Serial.available() > 0) {
-    int angle = Serial.parseInt();  // Read angle from serial input
-    right_front_lower.write(angle);         // Move servo to the specified angle
-    Serial.print("Servo moved to: ");
-    Serial.println(angle);
+    String input = Serial.readStringUntil('\n');
+    int jointPositions[12];  // Adjust array size based on your needs
+    
+    int index = 12;
+    char* token = strtok(&input[0], ",");
+    while (token != NULL && index < 3) {
+      jointPositions[index++] = atoi(token);
+      token = strtok(NULL, ",");
+    }
+
+    String result = "";
+    for (int i = 0; i <= 12; i++) {
+      result += String(jointPositions[i]);
+    }
+    Serial.print("moving to" + result);
+
+    for (int i = 0; i < index; i++) {
+      servos[i].write(jointPositions[i]);
+      const char* servo_name = servos_name[i];
+      Serial.print("Servo ");
+      Serial.print(servo_name);
+      Serial.print(" moved to: ");
+      Serial.println(jointPositions[i]);
+    }
   }
 
 }
--- master.py
+++ master.py
@@ -12,27 +12,49 @@ def check_com(com):
     else:
         print(f"Invalid COM port, COM available are:{ports}")
         raise ValueError("Invalid COM port")
-
-# com = input("Enter the COM port: ")
-# com = check_com(com)
-
-com = "/dev/cu.usbmodem11101"
-arduino = serial.Serial(port=com, baudrate=9600)
-time.sleep(2)
-
+    
+    
 def set_servo_angle(angle):
     print(f"Setting servo angle to {angle} degrees")
     if angle >= 0 and angle <= 180:
         arduino.write(f"{angle}".encode())  # Send the angle as a newline-terminated string
-        time.sleep(0.1)
         response = arduino.readline().decode().strip()  # Read Arduino's response
         print(response)
     else:
         print("Angle out of range (0-180)")
+        
+def set_joints_angles(angles: list):
+    message = ','.join(map(str, angles))
+    print(f"Setting servo angles to {message} degrees")
+    arduino.write(message.encode('utf-8'))
+    arduino.write(b'\n')  # Send newline as a terminator    
+    response = arduino.readline().decode().strip()  # Read Arduino's response
+    print(response)
 
-# Test by moving the servo to different angles
-for angle in range(0, 180, 10):
-    set_servo_angle(angle)
-    time.sleep(1)  # Wait 1 second between commands
+
+com = "/dev/cu.usbmodem11101"
+arduino = serial.Serial(port=com, baudrate=9600)
+time.sleep(2) # Wait for connection to happen
+
+# set_joints_angles([90, 90, 90, 90, 90, 90])
+
+
+#   left_back_upper.write(80);
+#   left_back_lower.write(110);
+#   set_constrained_feet_pos(30, left_back_feet);
+
+#   right_back_upper.write(100);
+#   right_back_lower.write(70);
+#   set_constrained_feet_pos(60, right_back_feet);
+
+#   left_front_upper.write(90);
+#   left_front_lower.write(50);
+#   set_constrained_feet_pos(60, left_front_feet);
+
+#   right_front_upper.write(90);
+#   right_front_lower.write(130);
+#   set_constrained_feet_pos(30, right_front_feet);
+
+set_joints_angles([80, 110, 30, 100, 70, 60, 90, 50, 60, 90, 130, 30])
 
 arduino.close()  # Close the serial connection
