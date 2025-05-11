090bdd8dbd7558ccb00a271daea9e9ec7bceae72
senecobis <ro.pellerito@libero.it>
2025-05-11 16:47:00 +0200

    Refactor leg testing functions and improve angle processing in main.py

--- main.py
+++ main.py
@@ -31,10 +31,10 @@ def postprocess_angles_minimal(angles):
             angles[ind] *= -1
         if angles[ind] >= 180:
             angles[ind] -= 180
-
     return angles
 
-if __name__ == "__main__":
+def test_front_right_leg(x_start=0, y_start=-l2-l3, z_start=-l1):
+    # Test the front right leg
     controller = ServoController()
     leg = Leg(l1, l2, l3, 
               slack_q1=-np.pi/2, slack_q2=0, slack_q3=0,
@@ -44,15 +44,64 @@ if __name__ == "__main__":
     line = list(np.linspace(0, l1, 100))    
     for i in line:
         time.sleep(0.1)
-        x = 0
-        # TODO debug why it doesn't rise the feet and it fails when we try to increase the 
-        # y coordinate
-        y = -l2-l3 + i*l3/l2
-        z = -l1 + i*20
+        x = x_start
+        y = y_start + i*l3/l2
+        z = z_start + i*20
         angles = leg.ik_pos(x, y, z)
         angles = postprocess_angles_minimal(angles)
         controller.move_front_right(angles[0], angles[1], angles[2])
         print(angles)
+
+def test_lower_right_leg(x_start=0, y_start=-l2-l3, z_start=-l1):
+    # Test the lower right leg
+    controller = ServoController()
+    leg = Leg(l1, l2, l3, 
+              slack_q1=-np.pi/2, slack_q2=0, slack_q3=0,
+              clockwise_q1=True, clockwise_q2=True, clockwise_q3=False
+              )
+    
+    line = list(np.linspace(0, l1, 100))    
+    for i in line:
+        time.sleep(0.1)
+        x = x_start
+        y = y_start + i*l3/l2
+        z = z_start + i*20
+        angles = leg.ik_pos(x, y, z)
+        angles = postprocess_angles_minimal(angles)
+        controller.move_lower_right(angles[0], angles[1], angles[2])
+        print(angles)
+        
+def test_right_legs(x_start=0, y_start=-l2-l3, z_start=-l1):
+    # Test the right legs
+    controller = ServoController()
+    leg_back = Leg(l1, l2, l3, 
+                slack_q1=-np.pi/2, slack_q2=0, slack_q3=0,
+                clockwise_q1=True, clockwise_q2=True, clockwise_q3=False
+                )
+    leg_front = Leg(l1, l2, l3,
+                slack_q1=-np.pi/2, slack_q2=0, slack_q3=0,
+                clockwise_q1=False, clockwise_q2=True, clockwise_q3=False
+                )
+    
+    line = list(np.linspace(0, l1, 100))    
+    for i in line:
+        time.sleep(0.1)
+        x = x_start
+        y = y_start + i*l3/l2
+        z = z_start + i*20
+        angles = leg_back.ik_pos(x, y, z)
+        angles = postprocess_angles_minimal(angles)
+        controller.move_lower_right(angles[0], angles[1], angles[2])
+        
+        angles = leg_front.ik_pos(x, y, z)
+        angles = postprocess_angles_minimal(angles)
+        controller.move_front_right(angles[0], angles[1], angles[2])
+        print(angles)
+
+if __name__ == "__main__":
+    test_front_right_leg()
+    # test_lower_right_leg()
+    # test_right_legs()
     
     
     
